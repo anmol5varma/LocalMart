c4abf612a1ea2515629ce4c56ce58ae153bdff92
Anmol Varma <anmol5varma@gmail.com>
2025-01-14 19:02:20 +0530

    chore: update mock product data.

--- src/constants.js
+++ src/constants.js
@@ -1,280 +1,218 @@
-export const CATEGORIES = ["dairy", "vegetables", "snacks", "soft drinks", "oil and ghee"];
+export const CATEGORIES = ["dairy", "vegetables", "snacks", "soft drinks", "oil and ghee", "home essentials"];
 
 export const PRODUCT_LIST = [
     {
         "id": "1",
-        "photoUrl": "link_to_photo1.jpg",
-        "title": "Product 1 Title",
-        "mrp": 100,
-        "category": "dairy"
-    },
-    {
-        "id": "2",
-        "photoUrl": "link_to_photo2.jpg",
-        "title": "Product 2 Title",
-        "mrp": 105,
-        "category": "vegetables"
-    },
-    {
-        "id": "3",
-        "photoUrl": "link_to_photo3.jpg",
-        "title": "Product 3 Title",
-        "mrp": 110,
-        "category": "snacks"
-    },
-    {
-        "id": "4",
-        "photoUrl": "link_to_photo4.jpg",
-        "title": "Product 4 Title",
-        "mrp": 115,
-        "category": "soft drinks"
-    },
-    {
-        "id": "5",
-        "photoUrl": "link_to_photo5.jpg",
-        "title": "Product 5 Title",
-        "mrp": 120,
+        "photoUrl": "https://shop.amul.com/s/62fa94df8c13af2e242eba16/65ba25a7a55405fa02e1881f/01-hero-image_amul-pure-ghee-tetrapack-1l.png",
+        "title": "Amul Pure Ghee",
+        "mrp": 500,
         "category": "oil and ghee"
     },
     {
+        "id": "2",
+        "photoUrl": "https://www.bigbasket.com/media/uploads/p/xxl/40147029_4-mother-dairy-toned-milk.jpg",
+        "title": "Mother Dairy Toned Milk",
+        "mrp": 48,
+        "category": "dairy"
+    },
+    {
+        "id": "3",
+        "photoUrl": "https://www.bigbasket.com/media/uploads/p/xl/50000557-2_5-fresho-tomato-hybrid-organically-grown.jpg",
+        "title": "Fresho Organic Tomatoes",
+        "mrp": 40,
+        "category": "vegetables"
+    },
+    {
+        "id": "4",
+        "photoUrl": "https://m.media-amazon.com/images/I/71bqdlAVGnL._AC_UF1000,1000_QL80_.jpg",
+        "title": "Lay's Classic Salted Chips",
+        "mrp": 20,
+        "category": "snacks"
+    },
+    {
+        "id": "5",
+        "photoUrl": "https://www.bigbasket.com/media/uploads/p/xl/251023_11-coca-cola-soft-drink-original-taste.jpg",
+        "title": "Coca-Cola Soft Drink",
+        "mrp": 40,
+        "category": "soft drinks"
+    },
+    {
         "id": "6",
-        "photoUrl": "link_to_photo6.jpg",
-        "title": "Product 6 Title",
-        "mrp": 125,
+        "photoUrl": "https://www.jiomart.com/images/product/original/490001397/britannia-cheese-cubes-200-g-carton-product-images-o490001397-p490001397-0-202306240857.jpg",
+        "title": "Britannia Cheese Cubes",
+        "mrp": 120,
         "category": "dairy"
     },
     {
         "id": "7",
-        "photoUrl": "link_to_photo7.jpg",
-        "title": "Product 7 Title",
-        "mrp": 130,
+        "photoUrl": "https://www.bigbasket.com/media/uploads/p/l/40057966_8-fresho-tender-coconut-medium.jpg",
+        "title": "Fresho Signature Tender Coconut",
+        "mrp": 35,
         "category": "vegetables"
     },
     {
         "id": "8",
-        "photoUrl": "link_to_photo8.jpg",
-        "title": "Product 8 Title",
-        "mrp": 135,
+        "photoUrl": "https://www.haldirams.com/media/catalog/product/cache/71134970afb779eb7860339989626b7e/b/h/bhujiya_sev_1.jpg",
+        "title": "Haldiram's Bhujia Sev",
+        "mrp": 85,
         "category": "snacks"
     },
     {
         "id": "9",
-        "photoUrl": "link_to_photo9.jpg",
-        "title": "Product 9 Title",
-        "mrp": 140,
+        "photoUrl": "https://www.bigbasket.com/media/uploads/p/xl/251006_13-sprite-soft-drink-lime-flavoured.jpg",
+        "title": "Sprite Lime Flavored Soft Drink",
+        "mrp": 40,
         "category": "soft drinks"
     },
     {
         "id": "10",
-        "photoUrl": "link_to_photo10.jpg",
-        "title": "Product 10 Title",
-        "mrp": 145,
+        "photoUrl": "https://m.media-amazon.com/images/I/81ILzuTdkbL.jpg",
+        "title": "Fortune Sunflower Oil",
+        "mrp": 150,
         "category": "oil and ghee"
     },
     {
         "id": "11",
-        "photoUrl": "link_to_photo11.jpg",
-        "title": "Product 11 Title",
-        "mrp": 150,
-        "category": "dairy"
+        "photoUrl": "https://m.media-amazon.com/images/I/81SJpjHr-TL.jpg",
+        "title": "Kellogg's Corn Flakes",
+        "mrp": 199,
+        "category": "snacks"
     },
     {
         "id": "12",
-        "photoUrl": "link_to_photo12.jpg",
-        "title": "Product 12 Title",
-        "mrp": 155,
-        "category": "vegetables"
+        "photoUrl": "https://m.media-amazon.com/images/I/71ZNcuBUV5L.jpg",
+        "title": "Tropicana Orange Juice",
+        "mrp": 99,
+        "category": "soft drinks"
     },
     {
         "id": "13",
-        "photoUrl": "link_to_photo13.jpg",
-        "title": "Product 13 Title",
-        "mrp": 160,
-        "category": "snacks"
+        "photoUrl": "https://www.bigbasket.com/media/uploads/p/xxl/1204532-7_3-yakult-probiotic-health-drink.jpg",
+        "title": "Yakult Probiotic Drink",
+        "mrp": 60,
+        "category": "dairy"
     },
     {
         "id": "14",
-        "photoUrl": "link_to_photo14.jpg",
-        "title": "Product 14 Title",
-        "mrp": 165,
-        "category": "soft drinks"
-    },
-    {
-        "id": "15",
-        "photoUrl": "link_to_photo15.jpg",
-        "title": "Product 15 Title",
-        "mrp": 170,
-        "category": "oil and ghee"
-    },
-    {
-        "id": "16",
-        "photoUrl": "link_to_photo16.jpg",
-        "title": "Product 16 Title",
-        "mrp": 175,
-        "category": "dairy"
-    },
-    {
-        "id": "17",
-        "photoUrl": "link_to_photo17.jpg",
-        "title": "Product 17 Title",
-        "mrp": 180,
-        "category": "vegetables"
-    },
-    {
-        "id": "18",
-        "photoUrl": "link_to_photo18.jpg",
-        "title": "Product 18 Title",
-        "mrp": 185,
+        "photoUrl": "https://m.media-amazon.com/images/I/617Y9Xm67eL._AC_UF1000,1000_QL80_.jpg",
+        "title": "Hershey's Chocolate Syrup",
+        "mrp": 190,
         "category": "snacks"
     },
     {
-        "id": "19",
-        "photoUrl": "link_to_photo19.jpg",
-        "title": "Product 19 Title",
-        "mrp": 190,
+        "id": "15",
+        "photoUrl": "https://i.imgur.com/Ep6FrjC.jpg",
+        "title": "Bisleri Mineral Water",
+        "mrp": 20,
         "category": "soft drinks"
     },
     {
-        "id": "20",
-        "photoUrl": "link_to_photo20.jpg",
-        "title": "Product 20 Title",
-        "mrp": 195,
+        "id": "16",
+        "photoUrl": "https://i.imgur.com/TWiRIzU.jpg",
+        "title": "Dabur Honey",
+        "mrp": 350,
+        "category": "snacks"
+    },
+    {
+        "id": "17",
+        "photoUrl": "https://i.imgur.com/NmoPPD3.jpg",
+        "title": "MTR Ready Mix Gulab Jamun",
+        "mrp": 105,
+        "category": "snacks"
+    },
+    {
+        "id": "18",
+        "photoUrl": "https://i.imgur.com/yUub4AD.jpg",
+        "title": "Patanjali Cow's Ghee",
+        "mrp": 575,
         "category": "oil and ghee"
     },
     {
+        "id": "19",
+        "photoUrl": "https://i.imgur.com/jQDyXpZ.jpg",
+        "title": "Kissan Mixed Fruit Jam",
+        "mrp": 130,
+        "category": "snacks"
+    },
+    {
+        "id": "20",
+        "photoUrl": "https://i.imgur.com/vE7P9Ie.jpg",
+        "title": "Maggi Masala Noodles",
+        "mrp": 12,
+        "category": "snacks"
+    },
+    {
         "id": "21",
-        "photoUrl": "link_to_photo21.jpg",
-        "title": "Product 21 Title",
-        "mrp": 200,
-        "category": "dairy"
+        "photoUrl": "https://i.imgur.com/y11NeY5.jpg",
+        "title": "Nescafé Classic Coffee",
+        "mrp": 290,
+        "category": "snacks"
     },
     {
         "id": "22",
-        "photoUrl": "link_to_photo22.jpg",
-        "title": "Product 22 Title",
-        "mrp": 205,
-        "category": "vegetables"
+        "photoUrl": "https://i.imgur.com/2sGT1sF.jpg",
+        "title": "Coca-Cola Zero Sugar",
+        "mrp": 45,
+        "category": "soft drinks"
     },
     {
         "id": "23",
-        "photoUrl": "link_to_photo23.jpg",
-        "title": "Product 23 Title",
-        "mrp": 210,
+        "photoUrl": "https://i.imgur.com/sfLsgq3.jpg",
+        "title": "Brooke Bond Red Label Tea",
+        "mrp": 330,
         "category": "snacks"
     },
     {
         "id": "24",
-        "photoUrl": "link_to_photo24.jpg",
-        "title": "Product 24 Title",
-        "mrp": 215,
-        "category": "soft drinks"
+        "photoUrl": "https://i.imgur.com/efGzupQ.jpg",
+        "title": "Britannia NutriChoice Digestive Biscuits",
+        "mrp": 50,
+        "category": "snacks"
     },
     {
         "id": "25",
-        "photoUrl": "link_to_photo25.jpg",
-        "title": "Product 25 Title",
-        "mrp": 220,
-        "category": "oil and ghee"
+        "photoUrl": "https://i.imgur.com/TqQjxxH.jpg",
+        "title": "Pedigree Dog Food Chicken & Vegetables",
+        "mrp": 270,
+        "category": "snacks"
     },
     {
         "id": "26",
-        "photoUrl": "link_to_photo26.jpg",
-        "title": "Product 26 Title",
+        "photoUrl": "https://i.imgur.com/IZbe1m5.jpg",
+        "title": "Surf Excel Matic Front Load Detergent Powder",
         "mrp": 225,
-        "category": "dairy"
+        "category": "home essentials"
     },
     {
         "id": "27",
-        "photoUrl": "link_to_photo27.jpg",
-        "title": "Product 27 Title",
-        "mrp": 230,
-        "category": "vegetables"
+        "photoUrl": "https://i.imgur.com/XKlQc3L.jpg",
+        "title": "Colgate MaxFresh Peppermint Ice Toothpaste",
+        "mrp": 95,
+        "category": "home essentials"
     },
     {
         "id": "28",
-        "photoUrl": "link_to_photo28.jpg",
-        "title": "Product 28 Title",
-        "mrp": 235,
-        "category": "snacks"
+        "photoUrl": "https://i.imgur.com/IOySvQS.jpg",
+        "title": "Harpic Power Plus Toilet Cleaner",
+        "mrp": 85,
+        "category": "home essentials"
     },
     {
         "id": "29",
-        "photoUrl": "link_to_photo29.jpg",
-        "title": "Product 29 Title",
-        "mrp": 240,
-        "category": "soft drinks"
+        "photoUrl": "https://i.imgur.com/h7rFbKb.jpg",
+        "title": "Lizol Disinfectant Floor Cleaner",
+        "mrp": 362,
+        "category": "home essentials"
     },
     {
         "id": "30",
-        "photoUrl": "link_to_photo30.jpg",
-        "title": "Product 30 Title",
-        "mrp": 245,
-        "category": "oil and ghee"
-    },
-    {
-        "id": "31",
-        "photoUrl": "link_to_photo21.jpg",
-        "title": "Product 31 Title",
-        "mrp": 200,
-        "category": "dairy"
-    },
-    {
-        "id": "32",
-        "photoUrl": "link_to_photo22.jpg",
-        "title": "Product 32 Title",
-        "mrp": 205,
-        "category": "vegetables"
-    },
-    {
-        "id": "33",
-        "photoUrl": "link_to_photo23.jpg",
-        "title": "Product 33 Title",
-        "mrp": 210,
-        "category": "snacks"
-    },
-    {
-        "id": "34",
-        "photoUrl": "link_to_photo24.jpg",
-        "title": "Product 34 Title",
-        "mrp": 215,
-        "category": "soft drinks"
-    },
-    {
-        "id": "35",
-        "photoUrl": "link_to_photo25.jpg",
-        "title": "Product 35 Title",
-        "mrp": 220,
-        "category": "oil and ghee"
-    },
-    {
-        "id": "36",
-        "photoUrl": "link_to_photo26.jpg",
-        "title": "Product 36 Title",
-        "mrp": 225,
-        "category": "dairy"
-    },
-    {
-        "id": "37",
-        "photoUrl": "link_to_photo27.jpg",
-        "title": "Product 37 Title",
-        "mrp": 230,
-        "category": "vegetables"
-    },
-    {
-        "id": "38",
-        "photoUrl": "link_to_photo28.jpg",
-        "title": "Product 38 Title",
-        "mrp": 235,
-        "category": "snacks"
-    },
-    {
-        "id": "39",
-        "photoUrl": "link_to_photo29.jpg",
-        "title": "Product 39 Title",
-        "mrp": 240,
-        "category": "soft drinks"
-    },
+        "photoUrl": "https://i.imgur.com/UW0DvnK.jpg",
+        "title": "Pampers Baby Dry Pants",
+        "mrp": 699,
+        "category": "home essentials"
+    }
 ]
+
 
 export const PHONE_NUMBER = "91<10 digit phone number for seller>"
 
